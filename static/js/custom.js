626e9967f0f50fbac13c2b724dbcdc7837ea39cf
LKY0506 <132634807+LKY0506@users.noreply.github.com>
2023-05-05 15:22:03 +0800

    Update custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -160,7 +160,7 @@ $(document).ready(function() {
     
     // 保存api key与对话数据
     let data = {
-      "apiKey" : "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", // 这里填写固定 apiKey
+      "apiKey" : "", // 这里填写固定 apiKey
     }
    
     // 判断是否使用自己的api key
